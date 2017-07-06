382927b322baa4b3af72c67bc307d603f4c9619d
Scott Ertel <jsecpa@gmail.com>
2017-07-05 21:07:10 -0500

    add methods to profile views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'profiles/index'
+  get 'profiles' => 'profiles#index'
 
-  get 'profiles/show'
+  get 'profiles/:id' => 'profiles#show', as: :profile
 
   devise_for :users
   resources :tweets
   root 'tweets#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
